b1611f5cd485fcea88bca4a3bb3110563d64303f
R4AYA4R <shilovich.yaroslav@gmail.com>
2024-12-03 13:04:36 +0200

    added full price filter input with type range

--- foodtuck-react/src/pages/Catalog.tsx
+++ foodtuck-react/src/pages/Catalog.tsx
@@ -4,18 +4,22 @@ import { useIsOnCreen } from "../hooks/useIsOnScreen";
 
 const Catalog = () => {
 
-    const [inputRightRangeValue, setInputRightRangeValue] = useState(124); // состояние для значения правого инпута с типом range,указываем начальное значение 124 в данном случае,чтобы изначально была заполнена вся полоска до ползунка у инпута с типом range
+    const [inputRightRangeValue, setInputRightRangeValue] = useState(0); // состояние для значения правого инпута с типом range,указываем начальное значение 124 в данном случае,чтобы изначально была заполнена вся полоска до ползунка у инпута с типом range
 
-    const [inputRightRangeTrackWidth, setInputRightRangeTrackWidth] = useState(124); // состояние для ширины полоски до ползунка у инпута с типом range,указываем начальное значение 124 в данном случае,чтобы изначально была заполнена вся полоска до ползунка у инпута с типом range
+    const [inputRightRangeTrackWidth, setInputRightRangeTrackWidth] = useState(0); // состояние для ширины полоски до ползунка у инпута с типом range,указываем начальное значение 124 в данном случае,чтобы изначально была заполнена вся полоска до ползунка у инпута с типом range
 
     const inputRightRangeRef = useRef<HTMLDivElement>(null); // переменная для ссылки на html элемент полоски до ползунка у инпута с типом range
 
+    const [inputRightRangePrice,setInputRightRangePrice] = useState(248); // состояние для значение цены,которое будет у правого инпута с типом range,будем его показывать как цену,а записывать в него будем значение с немного увеличенным значением как у самого инпута с типом range,чтобы это подходило под нашу цену(так как значение у самого инпута с типом range будет изменяться на минимальное расстояние как 1,а нам нужно,чтобы было больше чем 1 минимальное расстояние),ставим изначальное значение 248,чтобы сразу показывалось,что это максимальное значение цены
 
-    const [inputLeftRangeValue, setInputLeftRangeValue] = useState(124); // состояние для значения левого инпута с типом range,указываем начальное значение 124 в данном случае,чтобы изначально была заполнена вся полоска до ползунка у инпута с типом range
+
+    const [inputLeftRangeValue, setInputLeftRangeValue] = useState(0); // состояние для значения левого инпута с типом range,указываем начальное значение 124 в данном случае,чтобы изначально была заполнена вся полоска до ползунка у инпута с типом range
 
     const inputLeftRangeRef = useRef<HTMLDivElement>(null); // переменная для ссылки на html элемент полоски до ползунка у левого инпута с типом range
 
-    const [inputLeftRangeTrackWidth, setInputLeftRangeTrackWidth] = useState(124); // состояние для ширины полоски до ползунка у левого инпута с типом range,указываем начальное значение 124 в данном случае,чтобы изначально была заполнена вся полоска до ползунка у инпута с типом range
+    const [inputLeftRangeTrackWidth, setInputLeftRangeTrackWidth] = useState(0); // состояние для ширины полоски до ползунка у левого инпута с типом range,указываем начальное значение 124 в данном случае,чтобы изначально была заполнена вся полоска до ползунка у инпута с типом range
+
+    const [inputLeftRangePrice,setInputLeftRangePrice] = useState(0);  // состояние для значение цены,которое будет у левого инпута с типом range,будем его показывать как цену,а записывать в него будем значение с немного увеличенным значением как у самого инпута с типом range,чтобы это подходило под нашу цену(так как значение у самого инпута с типом range будет изменяться на минимальное расстояние как 1,а нам нужно,чтобы минимальное расстояние было больше чем 1)
 
 
 
@@ -73,7 +77,7 @@ const Catalog = () => {
 
         console.log(inputRightRangeValue);
 
-
+        setInputLeftRangePrice(+e.target.value * 0.404); // указываем значение состоянию цены для левого инпута с типом range(inputLeftRangePrice) как текущее значение этого инпута с типом range(e.target.value), также умножаем получившееся значение на 0.404,чтобы получить у этого правого инпута с типом range максимальное значение цены 50,делаем так,потому что ширина этого инпута 124 и минимальное изменение при смещении кружочка ползунка равно 1(типа 1 пикселю и в итоге получилось бы максимальное значение у инпута 124,а нам нужно 50),поэтому умножаем на 0.404(в данном случае просто подобрали примерно это число),чтобы уменьшить минимальное смещение этого инпута так,чтобы в итоге максимальным было 100, то есть в данном случае минимальное смещение у этого инпута было бы примерно 0.5 вместо 1
 
     }
 
@@ -125,7 +129,7 @@ const Catalog = () => {
         console.log(inputRightRangeValue);
 
 
-
+        setInputRightRangePrice((248 - +e.target.value) * 0.404); // указываем значение состоянию цены для левого инпута с типом range(inputLeftRangePrice) как 248(максимальное значение цены,которое должно быть в тексте на сайте ) - текущее значение этого инпута с типом range(e.target.value),чтобы от 248 отнималось текущее значение инпута с типом range при изменении текущего значения инпута с типом range,чтобы правильно показывалась цена, также умножаем получившееся значение на 0.404,чтобы получить у этого правого инпута с типом range максимальное значение цены 50,делаем так,потому что ширина этого инпута 124 и минимальное изменение при смещении кружочка ползунка равно 1(типа 1 пикселю и в итоге получилось бы максимальное значение у инпута 124,а нам нужно 50),поэтому умножаем на 0.404(в данном случае просто подобрали примерно это число),чтобы уменьшить минимальное смещение этого инпута так,чтобы в итоге максимальным было 100, то есть в данном случае минимальное смещение у этого инпута было бы примерно 0.5 вместо 1
     }
 
 
@@ -173,23 +177,25 @@ const Catalog = () => {
 
                                 <div className="filterBlock__priceInputs">
 
-                                    <div className="filterBlock__priceInputs-blockInput">
-                                        <input type="range" className="filterBlock__priceInputs-priceInput filterBlock__priceInputs-priceInputLeft" min="0" max="124" value={inputLeftRangeValue} onChange={OnChangeRangeLeft} />
+                                    <label className="filterBlock__priceInputs-blockInput" htmlFor="rangeLeft">
+                                        <input id="rangeLeft" type="range" className="filterBlock__priceInputs-priceInput filterBlock__priceInputs-priceInputLeft" min="0" max="124" value={inputLeftRangeValue} onChange={OnChangeRangeLeft} />
 
                                         {/* указываем этому диву в style в width значение как у состояния inputRangeTrackWidth(то есть состояние для полоски до ползунка у инпута с типом range),потом в коде выше изменяем это значение,соответственно и изменяем ширину этого div элемента(полоски до ползунка у инпута с типом range) */}
                                         <div className="priceInputs__blockInput-trackInput priceInputs__blockInput-trackInputLeft" style={{ "width": `${inputLeftRangeTrackWidth}px` }} ref={inputLeftRangeRef} ></div>
-                                    </div>
+                                    </label>
 
-                                    <div className="filterBlock__priceInputs-blockInput filterBlock__priceInputs-blockInputRight">
-                                        <input type="range" className="filterBlock__priceInputs-priceInput filterBlock__priceInputs-priceInputRight" min="0" max="124" value={inputRightRangeValue} onChange={OnChangeRangeRight}/>
+                                    <label htmlFor="rangeRight" className="filterBlock__priceInputs-blockInput filterBlock__priceInputs-blockInputRight">
+                                        <input id="rangeRight" type="range" className="filterBlock__priceInputs-priceInput filterBlock__priceInputs-priceInputRight" min="0" max="124" value={inputRightRangeValue} onChange={OnChangeRangeRight}/>
 
                                         {/* указываем этому диву в style в width значение как у состояния inputLeftRangeTrackWidth(то есть состояние для полоски до ползунка у левого инпута с типом range),потом в коде выше изменяем это значение,соответственно и изменяем ширину этого div элемента(полоски до ползунка у инпута с типом range) */}
                                         <div className="priceInputs__blockInput-trackInput priceInputs__blockInput-trackInputRight" style={{ "width": `${inputRightRangeTrackWidth}px` }} ref={inputRightRangeRef} ></div>
-                                    </div>
+                                    </label>
 
 
                                 </div>
-
+                                
+                                {/* используем здесь для текста цены метод toFixed(0),он указывает,сколько чисел может быть после запятой у числа,в данном случае указали 0 (чтобы было 0 чисел после запятой,также этот метод округляет автоматически число в большую сторону,когда это нужно,типа 1.6 округлит до 2,поэтому и указали более точное число(0.404),на которое умножали текущее значение инпута с типом range,чтобы этот метод toFixed в итоге правильно округлял числа),так как состояния inputLeftRangePrice(состояние цены для левого инпута с типом range) и inputRightRangePrice(состояние цены для правого инпута с типом range) могут получаться не целые при изменении ипнута с типом range */}
+                                <p className="filterBlockPrice__priceText">From ${inputLeftRangePrice.toFixed(0)} to ${inputRightRangePrice.toFixed(0)}</p>
                             </div>
                         </div>
                         <div className="sectionCatalog__main">
